ae6d99705c234a9b03ba328da16ba09a624e472b
Okky <okky.mstafford@gmail.com>
2025-09-05 10:37:33 +1000

    Destroy
    Created destroy (Delete) controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show edit update]
+  before_action :set_product, only: %i[show edit update destroy]
   def index
     @products = Product.all
   end
@@ -31,7 +31,9 @@ class ProductsController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
