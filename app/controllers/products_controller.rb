bd051075cfeb4132cbeafbb5c879fb14f1ee3862
Okky <okky.mstafford@gmail.com>
2025-09-07 14:29:32 +1000

    File Uploads with Active Storage
    Added feature to allow user to upload jpg and show it on page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   allow_unauthenticated_access only: %i[index show]
   before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @products = Product.all
   end
@@ -43,6 +44,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.expect(product: [ :name, :description ])
+    params.expect(product: [ :name, :description, :featured_image ])
   end
 end
